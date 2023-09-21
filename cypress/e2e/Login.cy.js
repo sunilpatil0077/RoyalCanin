7dfb4c37928c893405eca5ebe873b0ee1d691b26
sunilpatil0077 <144467476+sunilpatil0077@users.noreply.github.com>
2023-09-21 12:51:22 +0530

    Update Login.cy.js

--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -27,16 +27,6 @@ describe('RC Login', () =>
      cy.wait(20000)
      ln.LoginConfirmation()
 
-    /*cy.xpath("//button[@aria-label='My Account ']").click()
-    cy.xpath("//a[normalize-space()='Log in']").contains("Log in")
-    cy.xpath("//a[normalize-space()='Log in']").click()
-    cy.get("#onetrust-close-btn-container").click()
-    cy.get("#okta-signin-username").type("sunilpatil1308@gmail.com")
-    cy.get("#okta-signin-password").type("Sunil@007")
-    cy.get("#okta-signin-submit").click()
-    cy.xpath("//button[@aria-label='My Account Sunil']").click()
-    cy.xpath("//a[normalize-space()='Logout']").click()
-    cy.get("img[src='https://dl.royalcanin.com/8-12-1/logo--primary.svg']").should("be.visible").click()
-     */
+     
   })
 })
